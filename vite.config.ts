f50d066ef40beda02824319ff70e3f60c5892f18
Tomiwa Babatunde <tommybab7@gmail.com>
2026-04-27 10:36:12 -0700

    Remove conflicting TanStackRouterVite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
-    TanStackRouterVite({ autoCodeSplitting: false }),,
     react(),
     tailwindcss(),
     tsconfigPaths(),
