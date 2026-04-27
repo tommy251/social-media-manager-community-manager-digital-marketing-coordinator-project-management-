873c2d625b7825eabc6efe8ce94972c3b86e0e7e
Tomiwa Babatunde <tommybab7@gmail.com>
2026-04-27 10:04:38 -0700

    Add tanstackStart plugin and fix wrangler.jsonc

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { cloudflare } from "@cloudflare/vite-plugin";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
+    tanstackStart(),
     TanStackRouterVite({ autoCodeSplitting: true }),
     react(),
     tailwindcss(),
     tsconfigPaths(),
-    cloudflare(),
   ],
 });
